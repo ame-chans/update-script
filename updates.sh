cba3ae7569bd6f98d6ebad93904646703a1cae88
ame-chan <guillaumeemorine@outlook.com>
2025-07-29 17:42:15 +0200

    Updates to the script, minor fixes.

--- updates.sh
+++ updates.sh
@@ -7,18 +7,20 @@
 DATE=$(date -I)
 HOUR=$(date +%H)
 TIME=$(date +%R+%S)
-pkgs=$(cat /var/log/pacman.log | grep $DATE | grep $HOUR | grep "upgraded" | cut -d " " -f 4)
+pkgsatruntime=$(cat /var/log/pacman.log | grep $DATE | grep $HOUR | grep "upgraded" | cut -d " " -f 4)
+pkgstotal=$(cat /var/log/pacman.log | grep $DATE | grep "upgraded" | cut -d " " -f 4)
 
 echo "Updating packages: pacman"
 sudo pacman -Syyu --noconfirm
 echo "Updating packages: AUR"
 yay -Syyu --needed --noconfirm
 
-if [ -z "$pkgs" ] ; then
+if [ -z "${pkgs// }" ] ; then
 	notify-send -t 5000 "Update script" "No packages were updated at $TIME."
 	exit
 else
-	echo "The packages updated at" $TIME "are" $pkgs ". Please check https://archlinux.org/news/ for any manual interventions that may be required." > $HOME/pkgs-updated
+	echo "The packages updated at" $TIME "are" $pkgs ". Please check https://archlinux.org/news/ for any manual interventions that may be required !" > $HOME/pkgs-updated
 	pkgsnotify=$(cat $HOME/pkgs-updated)
 	notify-send -t 10000 "Update script" "$pkgsnotify"
+	notify-send -t 10000 "Update script" "Packages updated today: $pkgstotal"
 fi
