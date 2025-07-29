1a48f00f17b2ccf39f614b192855d215bc0b1ad1
ame-chan <guillaumeemorine@outlook.com>
2025-07-29 17:17:53 +0200

    Basic notification system, and time is now used as an indication of what's updated at what time ?

--- updates.sh
+++ updates.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 # A very, very barebones script that takes care of updating my packages on my install.
-# A .timer and .service file are also included, to let you enable and use it hourly, in order to not forget to update packages
-# To-Do : Eventually list packages that are updated (pacman logs), then also add a notification system ? Or try to, at least.
-# I'd ideally also add notifications for failures, or for anything that requires manual intervention, which would be a lot trickier.
+# A .timer and .service file are also included, to let you enable and use it hourly, in order to not forget to update packagesi
+# So far, we have notifications based on whether or not packages were updated.
+
+DATE=$(date -I)
+HOUR=$(date +%H)
+TIME=$(date +%R+%S)
+pkgs=$(cat /var/log/pacman.log | grep $DATE | grep $HOUR | grep "upgraded" | cut -d " " -f 4)
 
 echo "Updating packages: pacman"
 sudo pacman -Syyu --noconfirm
 echo "Updating packages: AUR"
 yay -Syyu --needed --noconfirm
+
+if [ -z "$pkgs" ] ; then
+	notify-send -t 5000 "Update script" "No packages were updated at $TIME."
+	exit
+else
+	echo "The packages updated at" $TIME "are" $pkgs ". Please check https://archlinux.org/news/ for any manual interventions that may be required." > $HOME/pkgs-updated
+	pkgsnotify=$(cat $HOME/pkgs-updated)
+	notify-send -t 10000 "Update script" "$pkgsnotify"
+fi
