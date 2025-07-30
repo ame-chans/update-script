2de95db35d5191881067e82c08cc38c8df9560c0
ame-chan <guillaumeemorine@outlook.com>
2025-07-30 14:27:47 +0200

    Changes to the script. Comments are added, and small changes done to the script itself.

--- updates.sh
+++ updates.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-# A very, very barebones script that takes care of updating my packages on my install.
-# A .timer and .service file are also included, to let you enable and use it hourly, in order to not forget to update packagesi
-# So far, we have notifications based on whether or not packages were updated.
+# A very, very barebones script that takes care of updating my packages on my install
+# A .timer and .service file are also included, to let you enable and use it hourly, in order to not forget to update packages
+# Very basic notifications system to inform, and there's a file that lists packages updated/upgraded
 
 DATE=$(date -I)
 HOUR=$(date +%H)
 TIME=$(date +%R+%S)
+
+# Wiping the contents of the files, regarding yesterday's contents
+echo "" > $HOME/pkgs-updated
+
+# Upgrading packages
+sudo pacman -Syyu --noconfirm
+yay -Syyu --needed --noconfirm
+
+# Obtaining the list of packages for the day, and even for the day and at the hour it's run
 pkgsatruntime=$(cat /var/log/pacman.log | grep $DATE | grep $HOUR | grep "upgraded" | cut -d " " -f 4)
 pkgstotal=$(cat /var/log/pacman.log | grep $DATE | grep "upgraded" | cut -d " " -f 4)
 
-echo "Updating packages: pacman"
-sudo pacman -Syyu --noconfirm
-echo "Updating packages: AUR"
-yay -Syyu --needed --noconfirm
-
+# Notification system, where it notifies based off the contents of the pkgs variable:
+# If the variable has an empty space (nothing is updated), then it displays a notification that states nothing was updated.
 if [ -z "${pkgs// }" ] ; then
 	notify-send -t 5000 "Update script" "No packages were updated at $TIME."
         notify-send -t 10000 "Update script" "Packages updated today: $pkgstotal"
@@ -24,3 +30,6 @@ else
 	notify-send -t 10000 "Update script" "$pkgsnotify"
 	notify-send -t 10000 "Update script" "Packages updated today: $pkgstotal"
 fi
+
+# Writing the packages of the day, all being written to a file so you can check on a daily basis.
+echo $DATE "-" $pkgstotal >> $HOME/pkgs-updated-total
